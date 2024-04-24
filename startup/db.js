9d3c2cf6e7eb70a50f99be825a0ed0deb810ac58
Hunter Carlis <huntercarlis@yahoo.com>
2024-04-24 16:20:05 -0700

    Add support for connection string via env var

--- startup/db.js
+++ startup/db.js
@@ -3,8 +3,11 @@ const winston = require('winston')
 const config = require('config')
 
 module.exports = function () {
-    const db = config.get('db');
-    mongoose.connect(db)
+    const db = config.get('db')
+    mongoose
+    .connect(config.get('db',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true }))
 .then(() => winston.info(`connected to ${db}`));
 
 }
